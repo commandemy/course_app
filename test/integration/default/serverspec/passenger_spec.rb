7130132894145ab7c1e5fccc9611d03f92493771
Edmund Haselwanter <me@ehaselwanter.com>
2015-01-29 00:19:19 +0100

    we need a login shell to get the path right

--- test/integration/default/serverspec/passenger_spec.rb
+++ test/integration/default/serverspec/passenger_spec.rb
@@ -5,7 +5,7 @@ set :backend, :exec
 
 describe 'Passenger' do
 
-  describe command('which passenger-status') do
+  describe command('bash -l -c "which passenger-status"') do
     its(:stdout) { should match '/opt/rbenv/shims/passenger-status' }
   end
 
